245e27bf4d7f71a0283d9b5cdb9839d084d26a6a
essejose <ze_pk2@hotmail.com>
2017-08-28 22:56:32 -0300

    working crab login

--- desenAula01/ViewController.swift
+++ desenAula01/ViewController.swift
@@ -8,20 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate{
 
-         override func viewDidLoad() {
+    @IBOutlet weak var tfName: UITextField!
+    
+    @IBOutlet weak var tfEmail: UITextField!
+    
+    @IBOutlet weak var tfPassword: UITextField!
+    
+    @IBOutlet weak var Avatar: UIImageView!
+    
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tfName.delegate = self;
+        
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        
+        if textField == tfName{
+        
+            tfEmail.becomeFirstResponder()
+        
+        }else if textField == tfEmail{
+            
+            tfPassword.becomeFirstResponder()
+            
+        }else{
+            
+            doSignUp()
+        }
+        
+        return true
     }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if  string.lowercased() != "j"{
+        
+        return true
+        }else{
+        return false
+        }
+        
+    }
+    
+    
+    
+        func textFieldDidEndEditing(_ textField: UITextField) -> Bool{
+        if textField == tfEmail && validateEmail(email: textField.text!){
+            
+            return false
+            
+        }else{
+            
+           return false
+        }
+    }
+    
+    
+    func validateEmail(email: String) -> Bool{
+        return false
+        
+    }
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
+    }
+    @IBAction func doSignUp() {
+        view.endEditing(true)
+        print("Fazendo cadastro")
+        
     }
 
 }
